37be29b3a655d63cc9eb39273e82d93ba323596a
za5dwunasta <magdalena.motyl@gmail.com>
2020-12-15 19:19:16 +0000

    Adding optimized assets

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 /* eslint-disable react/react-in-jsx-scope */
 import './App.css'
+import Header from './layouts/Header'
+import Home from './layouts/Home'
 
 function App () {
-  return <div className='App'>code</div>
+  return (
+    <div className='App'>
+      <Header />
+      <Home />
+    </div>
+  )
 }
 
 export default App
