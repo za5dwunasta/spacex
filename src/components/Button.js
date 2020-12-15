51bcff1b320b4d522277981e8d1a2ab14912ca03
za5dwunasta <magdalena.motyl@gmail.com>
2020-12-15 21:34:50 +0000

    adding button functionality

--- src/components/Button.js
+++ src/components/Button.js
@@ -6,6 +6,8 @@ import refresh from '../assets/icons/refresh.png';
 import select from '../assets/icons/select.png';
 import sort from '../assets/icons/sort.png';
 
+import { useLaunchesValue } from '../context/launches-context';
+
 const icons = {
 	refresh: refresh,
 	select: select,
@@ -13,8 +15,14 @@ const icons = {
 };
 
 function Button({ text, icon, eventHandler }) {
+	const { loading } = useLaunchesValue();
+
 	return (
-		<button onClick={eventHandler} className={'button' + (icon === 'refresh' ? ' button--refresh' : '')}>
+		<button
+			onClick={eventHandler}
+			disabled={loading}
+			className={'button' + (icon === 'refresh' ? ' button--refresh' : '')}
+		>
 			{text}
 			<img className="button__icon" src={icons[icon]} alt="" />
 		</button>
