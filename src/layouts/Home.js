18809c3e9183ce38ba7acda78f7b530a286bae84
za5dwunasta <magdalena.motyl@gmail.com>
2020-12-15 20:28:48 +0000

    set up Home page

--- src/layouts/Home.js
+++ src/layouts/Home.js
@@ -1,5 +1,17 @@
-import React from 'react'
+import React from 'react';
+import launchHome from '../assets/img/launch-home.png';
 
-export default function Home () {
-  return <div>Home</div>
+import './Home.scss';
+import Launches from '../components/Launches';
+
+export default function Home() {
+	return (
+		<div className="home">
+			<div className="home__image-container">
+				<img src={launchHome} alt="" className="home__landing-image" />
+			</div>
+
+			<Launches />
+		</div>
+	);
 }
